acc8d64035d184cab8cc9e1c49fa08a794fcfbb3
unknown <ignaciomentzelteper1@gmail.com>
2026-04-28 20:11:20 -0300

    header y footer

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,49 @@
+<div class="footer-container">
+    
+    <div class="footer-left">
+        <img src="http://centrodecomercioag.com.ar/wp-content/uploads/2023/07/cecit2023.png"><br><br>
+        <p>España 147, Alta Gracia, Córdoba, Argentina</p>
+        <p>Tel. (03547) 3547446368</p>
+    </div>
+
+    <div class="footer-right">
+        <div class="footer-logos">
+            <img src="http://centrodecomercioag.com.ar/wp-content/uploads/2025/04/Recurso-6-211x300.png">
+            <img src="http://centrodecomercioag.com.ar/wp-content/uploads/2025/04/Recurso-8-300x271.png">
+        </div>
+    </div>
+
+</div>
+
+<style>
+    .footer-container {
+    background: #0d1230;
+    color: white;
+    padding: 40px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.footer-left {
+    max-width: 50%;
+}
+
+.footer-left img {
+    width: 160px;
+}
+
+.footer-right {
+    text-align: center;
+}
+
+.footer-logos {
+    display: flex;
+    gap: 20px;
+    margin-top: 10px;
+}
+
+.footer-logos img {
+    width: 160px;
+}
+</style>
---
+++ src/lib/components/header.svelte
@@ -0,0 +1,57 @@
+<div class="header-container">
+    <div class="logo">
+        <img src="http://centrodecomercioag.com.ar/wp-content/uploads/2023/07/logonuevo.png">
+        <div>
+            <strong>CeCIT</strong><br>
+            <small>Somos Todos</small>
+        </div>
+    </div>
+
+    <nav class="nav">
+        <a href="#">Institucional</a>
+        <span>|</span>
+        <a href="#">Hacete socio</a>
+        <span>|</span>
+        <a href="#">Contacto</a>
+        <div class="user-icon"></div>
+    </nav>
+</div>
+
+<style>
+    .header-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: #ffffff;
+    padding: 10px 20px;
+    border-bottom: 2px solid rgb(253, 253, 253);
+}
+
+.logo {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+}
+
+.logo img {
+    width: 140px;
+}
+
+.nav {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+}
+
+.nav a {
+    text-decoration: none;
+    color: black;
+}
+
+.user-icon {
+    width: 30px;
+    height: 30px;
+    border: 2px solid black;
+    border-radius: 50%;
+}
+</style>  
