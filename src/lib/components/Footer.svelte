61a0eaffb56779cf3ffd2328ec0a803b325f1c2e
FedeLupianez <lupianezfederico@gmail.com>
2026-05-19 20:08:01 -0300

    fix white borders

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,34 +1,31 @@
 <footer>
-  <div>
-    <h2>CeCIT</h2>
+    <div>
+        <h2>CeCIT</h2>
 
-    <p>
-      España 147, Alta Gracia, Córdoba
-    </p>
-  </div>
+        <p>España 147, Alta Gracia, Córdoba</p>
+    </div>
 
-  <div>
-    <p>Nos respaldan</p>
-  </div>
+    <div>
+        <p>Nos respaldan</p>
+    </div>
 </footer>
 
 <style>
-  footer {
-    margin-top: 100px;
-
-    background: #151544;
-    color: white;
-
-    padding: 60px;
-
-    display: flex;
-    justify-content: space-between;
-  }
-
-  @media (max-width: 768px) {
     footer {
-      flex-direction: column;
-      gap: 20px;
+        margin-top: 100px;
+        background: #151544;
+        color: white;
+
+        padding: 60px;
+
+        display: flex;
+        justify-content: space-between;
     }
-  }
+
+    @media (max-width: 768px) {
+        footer {
+            flex-direction: column;
+            gap: 20px;
+        }
+    }
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,35 +1,26 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+    import favicon from "$lib/assets/favicon.svg";
 
-	import Navbar from '$lib/components/Navbar.svelte';
-	import Footer from '$lib/components/Footer.svelte';
+    import Navbar from "$lib/components/Navbar.svelte";
+    import Footer from "$lib/components/Footer.svelte";
 
-	let { children } = $props();
+    let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+    <link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="layout">
-	<Navbar />
+<Navbar />
 
-	<main>
-		{@render children()}
-	</main>
+<main>
+    {@render children()}
+</main>
 
-	<Footer />
-</div>
+<Footer />
 
 <style>
-	.layout {
-		min-height: 100vh;
-
-		display: flex;
-		flex-direction: column;
-	}
-
-	main {
-		flex: 1;
-	}
+    main {
+        flex: 1;
+    }
 </style>
