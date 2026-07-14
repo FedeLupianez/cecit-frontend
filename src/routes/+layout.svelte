74b7c8e495cac26da21fb5581bfc9f077bad7291
FedeLupianez <lupianezfederico@gmail.com>
2026-07-14 15:57:09 -0300

    perf: move refresh token and profile refresh login to +layout.server.ts to increase performance

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,66 +1,16 @@
 <script lang="ts">
-    import { accessToken } from "$lib/stores/authStore";
     import "../app.css";
 
     import Navbar from "$lib/components/Navbar.svelte";
     import Footer from "$lib/components/Footer.svelte";
-    import { onMount } from "svelte";
-    import { goto } from "$app/navigation";
-    import { profileStore } from "$lib/stores/profileStore";
+    import { profileStore } from "$lib/stores/profileStore.js";
+    import { accessToken } from "$lib/stores/authStore.js";
 
-    let { children } = $props();
-    let initialized = false;
-    let currentToken = $state(accessToken.getToken());
-
-    async function refreshProfile() {
-        const token = accessToken.getToken();
-        const res = await fetch("/api/auth/profile", {
-            method: "GET",
-            credentials: "include",
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
-        if (!res.ok) {
-            profileStore.clear();
-            return;
-        }
-        const data = await res.json();
-        profileStore.setProfile(data);
-    }
-
-    async function refresh() {
-        if (initialized) return;
-
-        const res = await fetch("/api/auth/refresh", {
-            method: "POST",
-            credentials: "include",
-        });
-
-        if (res.ok) {
-            const data = await res.json();
-            accessToken.setToken(data.access_token);
-            initialized = true;
-            return;
-        }
-        profileStore.clear();
-        accessToken.clear();
-
-        goto("/login");
-    }
+    let { data, children } = $props();
 
     $effect(() => {
-        if (currentToken) {
-            refreshProfile();
-        }
-    });
-
-    onMount(() => {
-        const unsub = accessToken.subscribe((value) => {
-            currentToken = value;
-        });
-        refresh();
-        return unsub;
+        profileStore.setProfile(data.profile);
+        accessToken.setToken(data.access_token);
     });
 </script>
 
